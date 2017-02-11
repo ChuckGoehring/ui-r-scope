38537a89b7b8de872a170368240376f45f789d19
Dan Kahle <daniel.kahle@acxiom.com>
2017-02-11 07:04:41 -0800

    move service code to parent controller

--- controllers.js
+++ controllers.js
@@ -1,12 +1,33 @@
+'use strict';
 
 angular.module('uirscope')
-   .controller('ctrlLeft', function ($scope, Base) {
+   .controller('ctrlHome', function ($scope) {
+      var base = this;
+      console.log('ctrlHome');
+
+      _.assign(base, {
+         sayHello: sayHello
+      });
+
+      init();
+
+      function init() {
+
+         base.data = {"name": "dank"};
+      }
+
+      function sayHello(msg) {
+         console.log('hello ' + base.data.name + ', ' + msg);
+      }
+
+   })
+   .controller('ctrlLeft', function ($scope) {
       var vm = this;
       vm.data = {msg: 'left msg'};
       console.log('ctrlLeft');
 
    })
-   .controller('ctrlRight', function ($scope, Base) {
+   .controller('ctrlRight', function ($scope) {
       var vm = this;
       vm.data = {msg: 'right msg'};
       console.log('ctrlRight');
